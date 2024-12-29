e31c87ffdd8d38f8b8de7ebed6e884d8312a2a13
Flynx <5723466+1p6@users.noreply.github.com>
2024-12-29 17:41:10 -0600

    make quat converge to a uniform side

--- rendering.gd
+++ rendering.gd
@@ -13,4 +13,5 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	($Quaternion/ColorRect.material as ShaderMaterial).set_shader_parameter('deltaTime', delta);
 	pass
