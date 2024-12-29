1f8bc5d58d0df725de6ba9eec7507e407fc1d7a3
Flynx <5723466+1p6@users.noreply.github.com>
2024-12-29 07:08:23 -0600

    Initial commit
    Got coordinate mapping between the cubemap shader and the main one
    working yay

---
+++ rendering.gd
@@ -0,0 +1,16 @@
+extends Node
+
+func onViewportChange():
+	$"World A".size = get_viewport().size
+	$"World B".size = get_viewport().size
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	onViewportChange()
+	get_viewport().size_changed.connect(onViewportChange)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
