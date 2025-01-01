856bb6197bf10597f83ca9de233bd5ea7c64cc81
Flynx <5723466+1p6@users.noreply.github.com>
2025-01-01 17:40:48 -0600

    more shapes

--- rendering.gd
+++ rendering.gd
@@ -63,5 +63,8 @@ func _process(delta: float) -> void:
 	($View.material as ShaderMaterial).set_shader_parameter('camPos', pos);
 	($View.material as ShaderMaterial).set_shader_parameter('camLook', camQuat);
 	($View.material as ShaderMaterial).set_shader_parameter('camLookRhs', rhsQuat);
-	$"Pos Look".text = "Pos: {0}\nLook: {1}".format([pos, camQuat * rhsQuat]);
+	var spinAxis = Vector2(Vector3(camQuat.x, camQuat.y, camQuat.z).length(), camQuat.w)
+	spinAxis = Vector2(spinAxis.x**2 - spinAxis.y**2, 2*spinAxis.x*spinAxis.y)
+	$"Pos Look".text = "Pos: {0}\nLook: {1}\nSpin: {2}".format(
+		[pos, camQuat * rhsQuat, spinAxis.angle()/TAU+0.5])
 	pass
