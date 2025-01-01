3b6b5d2b98420fd75ce3942b361e9cfd7c27560c
Flynx <5723466+1p6@users.noreply.github.com>
2025-01-01 11:03:28 -0600

    lots more 4D stuff
    phong reflection, separate quaternionic and 3D rotation controls,
    clifford torus, uhhh probably some other things i forgot about

--- rendering.gd
+++ rendering.gd
@@ -2,16 +2,23 @@ extends Node
 class_name Renderer
 
 var camQuat := Quaternion.IDENTITY
+var rhsQuat := Quaternion.IDENTITY
 var pos := Quaternion(0,0,0,0)
-const SENSITIVITY := 1.0/250
+const SENSITIVITY := 1.0/125
 const MOVE_SPEED := 5.0
 const ROLL_SPEED := 1.0
+
+func updateLook(rel: Quaternion) -> void:
+	camQuat = camQuat * rel
+	if Input.is_action_pressed("keep_slice"):
+		# adjust rhsQuat so that rel * Quaternion(0,0,1,0) * rhs_rel = Quaternion(0,0,1,0)
+		rhsQuat = Quaternion(0,0,-1,0) * rel.inverse() * Quaternion(0,0,1,0) * rhsQuat
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		var dir = event.screen_relative * SENSITIVITY
 		dir = Vector3.RIGHT*dir.x + Vector3.DOWN*dir.y + Vector3.FORWARD
-		camQuat = camQuat * Quaternion(Vector3.FORWARD, dir.normalized())
+		updateLook(Quaternion(Vector3.FORWARD, dir.normalized()))
 	elif event is InputEventMouseButton:
 		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	elif event.is_action_pressed("quit"):
@@ -33,8 +40,9 @@ func _process(delta: float) -> void:
 	var rollAmount := 0.0
 	if Input.is_action_pressed("roll_ccw"): rollAmount += 1.0
 	if Input.is_action_pressed("roll_cw"): rollAmount -= 1.0
-	camQuat = camQuat * Quaternion(Vector3.FORWARD, rollAmount*ROLL_SPEED*delta)
+	updateLook(Quaternion(Vector3.FORWARD, rollAmount*ROLL_SPEED*delta))
 	camQuat = camQuat.normalized()
+	rhsQuat = rhsQuat.normalized()
 
 	var moveDir = Quaternion(0,0,0,0)
 	if Input.is_action_pressed("move_forward"):
@@ -51,7 +59,9 @@ func _process(delta: float) -> void:
 		moveDir += Quaternion(-1,0,0,0)
 	if moveDir.length_squared() > 0.1:
 		moveDir = moveDir.normalized()
-		pos += camQuat * moveDir * MOVE_SPEED * delta
+		pos += camQuat * moveDir * rhsQuat * (MOVE_SPEED * delta)
 	($View.material as ShaderMaterial).set_shader_parameter('camPos', pos);
 	($View.material as ShaderMaterial).set_shader_parameter('camLook', camQuat);
+	($View.material as ShaderMaterial).set_shader_parameter('camLookRhs', rhsQuat);
+	$"Pos Look".text = "Pos: {0}\nLook: {1}".format([pos, camQuat * rhsQuat]);
 	pass
