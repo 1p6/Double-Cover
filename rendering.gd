673762c3fbd05e4d85af0c556f6f0fbe194bc1e9
Flynx <5723466+1p6@users.noreply.github.com>
2025-01-01 07:21:00 -0600

    initial seemingly working 4D version
    there might be a bug with the cube sdf, not sure

--- rendering.gd
+++ rendering.gd
@@ -1,34 +1,57 @@
 extends Node
 class_name Renderer
 
-var isOnBSide : bool = false
-var doneFirstSideChangeEmit: bool = false
-signal onSideChange(newIsOnBSide: bool)
+var camQuat := Quaternion.IDENTITY
+var pos := Quaternion(0,0,0,0)
+const SENSITIVITY := 1.0/250
+const MOVE_SPEED := 5.0
+const ROLL_SPEED := 1.0
 
-func onViewportChange():
-	$"World A".size = get_viewport().size
-	$"World B".size = get_viewport().size
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		var dir = event.screen_relative * SENSITIVITY
+		dir = Vector3.RIGHT*dir.x + Vector3.DOWN*dir.y + Vector3.FORWARD
+		camQuat = camQuat * Quaternion(Vector3.FORWARD, dir.normalized())
+	elif event is InputEventMouseButton:
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	elif event.is_action_pressed("quit"):
+		get_tree().quit()
+
+func viewportChanged() -> void:
+	($View.material as ShaderMaterial).set_shader_parameter("screenRes",
+			get_viewport().size)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	onViewportChange()
-	get_viewport().size_changed.connect(onViewportChange)
-	pass # Replace with function body.
+	get_viewport().size_changed.connect(viewportChanged)
+	viewportChanged()
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	($Quaternion/ColorRect.material as ShaderMaterial).set_shader_parameter('deltaTime', delta);
+	var rollAmount := 0.0
+	if Input.is_action_pressed("roll_ccw"): rollAmount += 1.0
+	if Input.is_action_pressed("roll_cw"): rollAmount -= 1.0
+	camQuat = camQuat * Quaternion(Vector3.FORWARD, rollAmount*ROLL_SPEED*delta)
+	camQuat = camQuat.normalized()
+
+	var moveDir = Quaternion(0,0,0,0)
+	if Input.is_action_pressed("move_forward"):
+		moveDir += Quaternion(0,0,0,1)
+	if Input.is_action_pressed("move_backward"):
+		moveDir += Quaternion(0,0,0,-1)
+	if Input.is_action_pressed("move_left"):
+		moveDir += Quaternion(0,1,0,0)
+	if Input.is_action_pressed("move_right"):
+		moveDir += Quaternion(0,-1,0,0)
+	if Input.is_action_pressed("move_up"):
+		moveDir += Quaternion(1,0,0,0)
+	if Input.is_action_pressed("move_down"):
+		moveDir += Quaternion(-1,0,0,0)
+	if moveDir.length_squared() > 0.1:
+		moveDir = moveDir.normalized()
+		pos += camQuat * moveDir * MOVE_SPEED * delta
+	($View.material as ShaderMaterial).set_shader_parameter('camPos', pos);
+	($View.material as ShaderMaterial).set_shader_parameter('camLook', camQuat);
 	pass
-
-
-func _on_side_var_updater_timeout() -> void:
-	var vp: SubViewport = $Quaternion;
-	var img = vp.get_texture().get_image();
-	var center = img.get_pixel(img.get_width()/2, img.get_height()/4);
-	var prevSide = isOnBSide
-	isOnBSide = center.b > 0.5;
-	if prevSide != isOnBSide or not doneFirstSideChangeEmit:
-		onSideChange.emit(isOnBSide)
-		doneFirstSideChangeEmit = true
-	$Label.text = "Side: " + ("B" if isOnBSide else "A");
